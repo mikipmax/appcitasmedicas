10f2d717deab8e8bfc29b26c5ec6917d98c12b0e
mikipmax <pmax-111@hotmail.es>
2021-04-18 22:04:47 -0500

    Comentado

--- app/src/main/java/uce/edu/ec/fing/proxies/ProducerReservacionProxy.java
+++ app/src/main/java/uce/edu/ec/fing/proxies/ProducerReservacionProxy.java
@@ -1,5 +1,21 @@
 package uce.edu.ec.fing.proxies;
+/*
+***UNIVERSIDAD CENTRAL DEL ECUADOR***
+***FACULTAD DE INGENIERIA Y CIENCIAS APLICADAS***
+***CARRERAS: INGENIERÍA EN COMPUTACIÓN GRÁFICA​E INGENIERÍA INFORMÁTICA***
+***MATERIA: DISPOSITIVOS MÓVILES***
 
+Grupo N: 5
+Integrantes:  -Nicolalde Estefanía ​Correo: jenicolaldep@uce.edu.ec
+              -Ponce Michael​​Correo: mfponce@uce.edu.ec
+              -Sánchez Jonathan​​Correo: jjsanchezl1@uce.edu.ec
+              -Tituaña Mayra​Correo: mrtituana@uce.edu.ec
+Descripción: La aplicación DATAMED se manifiesta de forma similar a una agenda, y tiene el propósito
+*            de agilizar el proceso de consulta. Consume microservicios alojados en un servidor de
+*            cloud gratuito, permite a los administrativos registrar las citas médicas de
+*            los pacientes brindando la facilidad de gestionar parámetros como: la fecha,
+*            especialista, área y tipo de pago.
+*/
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import uce.edu.ec.fing.utils.Util;
